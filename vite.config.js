604985fb192aa31d4163ef171a3f7cf91ae4d20e
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-06 18:38:58 -0400

    initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-const path = require("path");
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  root: path.resolve(__dirname, "src"),
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-    },
-  },
-  server: {
-    port: 8080,
-    hot: true,
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
