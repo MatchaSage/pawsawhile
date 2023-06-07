6ce41274923de55ffc65eb9659f2f91badb5f95e
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-07 14:40:42 -0400

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact.jsx";
+import Grooming from "./components/Grooming.jsx";
+import Home from "./components/Home.jsx";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React with ypescript + Bootstrap 5</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button variant="primary">Primary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>{" "}
-      <Button variant="success">Success</Button>{" "}
-      <Button variant="warning">Warning</Button>{" "}
-      <Button variant="danger">Danger</Button>{" "}
-      <Button variant="info">Info</Button>{" "}
-      <Button variant="light">Light</Button>{" "}
-      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Grooming" element={<Grooming />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
