dabb2f4b7af15876d6430a3bf8907b9d96325a51
CrosscutX <crosscutcode@gmail.com>
2023-06-08 14:50:43 -0400

    Component setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
-import Contact from "./components/Contact.jsx";
-import Grooming from "./components/Grooming.jsx";
-import Home from "./components/Home.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Grooming from "./components/grooming/Grooming.jsx";
+import Home from "./components/cemetary/Home.jsx";
 
 export default function App() {
   return (
