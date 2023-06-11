ae2cbf664d5ffe73c961e982b35b01626181adef
CrosscutX <crosscutcode@gmail.com>
2023-06-11 19:20:45 -0400

    Refresh bug fix attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./components/contact/Contact.jsx";
@@ -6,13 +6,22 @@ import Grooming from "./components/grooming/Grooming.jsx";
 import Home from "./components/cemetary/Home.jsx";
 
 export default function App() {
-  return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Grooming" element={<Grooming />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-    </div>
-  );
+  const [hasMounted, setHasMounted] = useState(false);
+  //Use effect concept to fix the refresh bug.
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  } else {
+    return (
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Grooming" element={<Grooming />} />
+          <Route path="Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    );
+  }
 }
