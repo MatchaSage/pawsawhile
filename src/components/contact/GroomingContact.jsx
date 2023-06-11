ea2ee76ad336b13c0501bb35cdf41738ef828deb
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-11 19:08:19 -0400

    Contact page skeleton and form setup

--- src/components/contact/GroomingContact.jsx
+++ src/components/contact/GroomingContact.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+export default function groomContact() {
+  return (
+    <div className="grooming-contact-container">
+      <h1>CJ's Salon & Lael's Groom Room</h1>
+      <span>Phone: </span>
+      <span>Hours: Tues-Sat By Appointment</span>
+    </div>
+  );
+}
