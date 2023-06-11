c19c7dd4653524c296597e1ca275e2d7354cc00f
CrosscutX <crosscutcode@gmail.com>
2023-06-11 18:35:50 -0400

    Youtube embed bug fixes

--- src/components/cemetary/K9Memorial.jsx
+++ src/components/cemetary/K9Memorial.jsx
@@ -7,7 +7,7 @@ export default function K9Memorial() {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/peqWqkcGAmM"
+        src="https://www.youtube-nocookie.com/embed/peqWqkcGAmM"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
