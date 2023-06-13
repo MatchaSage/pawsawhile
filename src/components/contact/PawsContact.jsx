8964620541684dcc72c48cfe39a9ebb487f48d6a
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-13 05:00:51 -0400

    Contact page setup

--- src/components/contact/PawsContact.jsx
+++ src/components/contact/PawsContact.jsx
@@ -6,23 +6,23 @@ export default function pawsContact() {
       <h1>Paws Awhile</h1>
       <span>Hours: By Appointment</span>
       <div
-        className="paws-contact--form h-75 w-75 mask px-5 py-5"
+        className="paws-contact--form mask"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
       >
         <form className="d-flex flex-column">
-          <label htmlFor="name" className="mt-3">
+          <label htmlFor="name" className="pt-2">
             Name:{" "}
           </label>
-          <input type="text" id="name" name="name" className="w-25"></input>
-          <label htmlFor="phoneNumber" className="mt-3">
+          <input type="text" id="name" name="name" className=""></input>
+          <label htmlFor="phoneNumber" className="pt-2">
             Number:{" "}
           </label>
-          <input type="tel" id="number" name="number" className="w-25"></input>
-          <label htmlFor="email" className="mt-3">
+          <input type="tel" id="number" name="number" className=""></input>
+          <label htmlFor="email" className="pt-2">
             Email:{" "}
           </label>
-          <input type="email" id="email" name="email" className="w-25"></input>
-          <label htmlFor="message" className="mt-3">
+          <input type="email" id="email" name="email" className=""></input>
+          <label htmlFor="message" className="pt-2">
             Message:{" "}
           </label>
           <textarea
@@ -34,7 +34,7 @@ export default function pawsContact() {
           ></textarea>
           <input
             type="submit"
-            value={"submit"}
+            value={"Submit"}
             className="mt-5 btn btn-primary submit-button"
           ></input>
         </form>
