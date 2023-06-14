dbe636ac48d5d55b0458da190584b0941624c6af
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-14 15:18:05 -0400

    changes

--- src/components/grooming/GroomingProposition.jsx
+++ src/components/grooming/GroomingProposition.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function groomingProposition() {
   return (
-    <div className="proposition-container d-flex flex-column align-items-center pt-5 pb-5">
+    <div className="proposition-container d-flex flex-column align-items-center pt-5 pb-5 p-5">
       <p className="business-proposition-text w-75 pb-3">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, magnam
         consectetur doloremque quidem alias sit nemo laborum. Nobis reiciendis
