c329ecae82ffe33550e2beb1810a78e6978b5321
CrosscutX <crosscutcode@gmail.com>
2023-06-13 22:39:26 -0400

    First working home prototype

--- src/components/cemetary/AboutJoe.jsx
+++ src/components/cemetary/AboutJoe.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Bear from "../../images/bear";
+import Bear from "../../images/bear.jpg";
 
 export default function AboutJoe() {
   return (
-    <div className="about-joe d-flex flex-column align-items-center">
-      <h1>About</h1>
-      <div className="about-joe-content d-flex justify-content-between">
-        <img
-          src={Bear}
-          alt="Joe Picture"
-          style={{ height: "200px", maxWidth: "50%" }}
-        />
+    <div className="about-joe d-flex flex-column align-items-center p-5">
+      <h1 className="mb-4">About Joe</h1>
+      <div className="about-joe-content d-flex flex-md-row flex-column justify-content-center">
+        <img src={Bear} alt="Joe Picture" />
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
           officiis cum eos magni iste quam excepturi vel? Enim consectetur in,
--- src/components/cemetary/PawsGrounds.jsx
+++ src/components/cemetary/PawsGrounds.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-
+import grounds1 from "../../images/grounds1.jpg";
 export default function PawsGrounds() {
   return (
     <div className="paws-grounds">
-      <div className="grounds-background h-100">
-        <div className="grounds-content text-white d-flex flex-column justify-content-center align-items-center h-100">
+      <div className="grounds-background p-5">
+        <div className="grounds-content text-dark d-flex flex-column justify-content-start align-items-center">
           <h1 className=" ">Grounds</h1>
-          <p>
+          <p className="mb-5">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda,
             corrupti in. Sint velit earum, quia sequi commodi placeat veritatis
             ducimus quam ipsam consequuntur hic magnam modi obcaecati maxime
             ipsum amet!
           </p>
+          <img src={grounds1} alt="" />
         </div>
       </div>
     </div>
