3bf4cf63ee34d589dca86f303f41b84241dc1b6e
Treyce Wheeler <t.zeryon@gmail.com>
2023-06-18 22:38:30 -0400

    Fixed page not scrolling to top on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Contact from "./components/contact/Contact.jsx";
 import Grooming from "./components/grooming/Grooming.jsx";
 import Home from "./components/cemetary/Home.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 export default function App() {
   const [hasMounted, setHasMounted] = useState(false);
 
   return (
     <div className="app">
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="Grooming" element={<Grooming />} />
