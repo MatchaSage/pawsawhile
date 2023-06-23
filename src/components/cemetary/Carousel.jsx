c9abf0ce34ee7c4670abdf7550e94a3f266cc138
CrosscutX <crosscutcode@gmail.com>
2023-06-23 19:46:32 -0400

    fix for carousel images

--- src/components/cemetary/Carousel.jsx
+++ src/components/cemetary/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import grounds1 from "../../images/grounds1.jpg";
 import grounds2 from "../../images/grounds2.jpg";
 import grounds3 from "../../images/grounds3.jpg";
@@ -7,6 +7,11 @@ import grounds4 from "../../images/grounds4.jpg";
 export default function Carousel() {
   const [length, setLength] = useState(0);
   const groundsArray = [grounds1, grounds2, grounds3, grounds4];
+  //Loads all carousel images early for a more responsive experience.
+  groundsArray.forEach((picture) => {
+    const img = new Image();
+    img.src = picture.jpg;
+  });
 
   function incrementLength() {
     if (length < 3) {
