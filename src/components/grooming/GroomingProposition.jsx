55d918565746ac6baccdb7cd41e13a30be65aa09
CrosscutX <crosscutcode@gmail.com>
2023-06-25 19:50:13 -0400

    button fix, first production build

--- src/components/grooming/GroomingProposition.jsx
+++ src/components/grooming/GroomingProposition.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function groomingProposition() {
   return (
     <div className="proposition-container d-flex flex-column align-items-center pt-5 pb-5 p-5">
@@ -7,9 +7,13 @@ export default function groomingProposition() {
         If you are interested in getting grooming care from proven professionals
         with 20 plus years of experience, be sure to give us a call!
       </p>
-      <a className="btn btn-outline-light btn-lg" href="#!" role="button">
+      <Link
+        className="btn btn-outline-light btn-lg"
+        to="/Contact"
+        role="button"
+      >
         Get in Touch
-      </a>
+      </Link>
     </div>
   );
 }
