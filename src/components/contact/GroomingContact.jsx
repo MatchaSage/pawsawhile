c903463f30a8b095fc8bb3fa2cc4723a6a55cbae
CrosscutX <crosscutcode@gmail.com>
2023-07-18 17:58:59 -0400

    text fix

--- src/components/contact/GroomingContact.jsx
+++ src/components/contact/GroomingContact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function groomContact() {
   return (
     <div className="grooming-contact-container d-flex flex-column align-items-center my-5 p-5">
-      <h1>CJ's Salon & Lael's Groom Room</h1>
+      <h1>CJ's Salon</h1>
       <span className="grooming-contact--text">
         <b>Phone: </b>
         330-321-1520
